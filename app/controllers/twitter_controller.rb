c599e4ad507be319a7426ab85fc7fab05f67a288
tusharmaroo <tusharmaroo@gmail.com>
2016-08-10 16:10:49 +0530

    Adding controller + Views

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,29 @@
+class TwitterController < ApplicationController
+
+  before_action :create_service_instance, only: [:result]
+
+  def index
+    # poor index page
+  end
+
+  def result
+    if params[:username].present? || params[:userid].present?
+      user_reference = params[:username] || params[:userid]
+      begin
+        @best_day_to_tweet, @best_time_to_tweet = @api_obj.calculate_best_tweet_time(user_reference)
+      rescue
+        Rails.logger.info "Tech Issue: Current Time: #{Time.zone.now}, user_reference: #{user_reference}"
+      end
+    end
+  end
+
+  private
+    def create_service_instance
+      begin
+        @api_obj = TwitterService.new
+        rescue
+          Rails.logger.info "fsdfsfdf"
+        end
+    end
+
+end
